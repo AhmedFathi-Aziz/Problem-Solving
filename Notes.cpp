15894a2c8988eba676830b0d658a3120e5501435
AhmedFathi <ahmed.fathi.aziz@gmail.com>
2021-08-23 22:20:01 +0200

    Problems

--- Notes.cpp
+++ Notes.cpp
@@ -1,6 +1,13 @@
 /* 
     Let's Go
-    
+
     Day1:
     be careful with the spelling
+
+    Time Complexity of Merge Sort:      Best:          Average:        Worst
+                                     O(n log(n))      O(n log(n))     O(n log(n))
+
+    Time Complexity of Bubble Sort:   Best:            Average        Worst
+                                      O(n)              O(n^2)        O(n^2)
+
 */
