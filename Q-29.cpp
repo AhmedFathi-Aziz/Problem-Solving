e1c034e5be1953a10e6d51e3a14782e25e3b7377
AhmedFathi <ahmed.fathi.aziz@gmail.com>
2021-08-26 06:24:43 +0200

    Problems

--- Q-29.cpp
+++ Q-29.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 int main()
 {
-    int length, distinct;
-    cin >> length >> distinct;
+    // int length, distinct;
+    // cin >> length >> distinct;
 
-    string password;
-    int in = distinct;
-    while (password.length() < length)
-    {
-        password += char('`' + in);
-        in--;
-        if (in == 0)
-            in = distinct;
-    }
-    cout << password << endl;
+    // string password;
+    // int in = distinct;
+    // while (password.length() < length)
+    // {
+    //     password += char('`' + in);
+    //     in--;
+    //     if (in == 0)
+    //         in = distinct;
+    // }
+    // cout << password << endl;
+
+    int n, in;
+    cin >> n >> in;
+
+    for (int i = 0; i < n; i++)
+        cout << char('a' + i % in);
 }
